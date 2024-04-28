be13c83f74e168f33ff301d91217adb367b37754
firasyazid <98762038+firasyazid@users.noreply.github.com>
2024-04-28 15:46:33 +0100

    push tests 2

--- Appointement/test/app.test.js
+++ Appointement/test/app.test.js
@@ -1,7 +1,34 @@
 const request = require('supertest');
 const app = require('../index');  
  
- 
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+
+////test db connection
+beforeAll(async () => {
+  await mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "Health_App",
+  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+describe("Database Connection", () => {
+  test("Should connect to the database", async () => {
+    await new Promise((resolve) => {
+      mongoose.connection.on("connected", () => {
+        resolve();
+      });
+    });
+
+    expect(mongoose.connection.readyState).toBe(1);
+  });
+});
 describe('GET /appointement', () => {
   test('should return all appointments', async () => {
     const response = await request(app)
--- medecin/test/medecin.test.js
+++ medecin/test/medecin.test.js
@@ -5,8 +5,37 @@ const request = require('supertest');
 const app = require('../index');
 const Medecin = require('../models/medecin');
 
+
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+
+////test db connection
+beforeAll(async () => {
+  await mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "Health_App",
+  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+describe("Database Connection", () => {
+  test("Should connect to the database", async () => {
+    await new Promise((resolve) => {
+      mongoose.connection.on("connected", () => {
+        resolve();
+      });
+    });
+
+    expect(mongoose.connection.readyState).toBe(1);
+  });
+});
   
-describe('GET /appointement', () => {
+describe('GET /medecin', () => {
     test('should return all medecins', async () => {
       const response = await request(app)
         .get('/api/v1/medecin');
